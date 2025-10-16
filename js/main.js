daf52320d548a95069fbf2831a8410e9013ef622
Shailendra Kumar Pandey <shailendrakr2010@gmail.com>
2025-10-16 22:27:52 +0530

    uplode file in project

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// News API and fetch data from it and display it in the browser
+
+const newsKey = "b73842219062446de118899d3359f40c"
+
+// let page = 1;
+// let url = `https://gnews.io/api/v4/top-headlines?category=sports&lang=en&country=in&max=10&page=${page}&apikey=${newsKey}`
+
+fetchNews("general");
+
+function fetchNews(category) {
+    fetch(`https://gnews.io/api/v4/top-headlines?category=${category}&lang=en&country=in&max=10&apikey=${newsKey}`)
+    .then((res)=>{
+        return res.json();
+    })
+    .then((res)=>{
+        document.getElementById("title").innerText = category.toUpperCase();
+        loadNews(res)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+}
+
+function loadNews(res) {
+    let totalRow = ""
+    for (let i = 0; i < res.articles.length-1; i++) {       
+        let row =  `
+                                <div class="card">
+                                    <div class="img">
+                                        <img src="${res.articles[i].image}" alt="img">
+                                    </div>
+                                    <div class="content">
+                                        <h4>${(res.articles[i].title).slice(0,100)}...</h4>
+                                        <p>${(res.articles[i].description.slice(0,200))}...</p>
+                                        <button href="#">Read more...</button>
+                                    </div>
+                                </div>
+        `
+        totalRow += row;
+    }
+    document.getElementById("newsAccordian").innerHTML = totalRow;
+}
